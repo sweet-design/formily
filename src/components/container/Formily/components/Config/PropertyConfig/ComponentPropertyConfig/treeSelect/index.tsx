f46d86e4193aa2cd6865fdf358e65bc6086c1e7c
lebron0801 <zhulei0801@163.com>
2022-08-05 15:12:58 +0800

    feat(tree-select component): add tree -select component
    * feat: add tree-select config
    
    * feat: add tree-select component view
    
    * fix: fix other component bug

---
+++ src/components/container/Formily/components/Config/PropertyConfig/ComponentPropertyConfig/treeSelect/index.tsx
@@ -0,0 +1,392 @@
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+import DataTypeSwitch from '../../../../DataTypeSwitch';
+
+@Component
+export default class TreeSelect extends Vue {
+  /**
+   * 所有配置数据
+   */
+  @Prop({
+    type: Object,
+    default: () => {},
+  })
+  data!: any;
+
+  /**
+   * 当前选中的组件配置数据模型
+   */
+  @Prop({
+    type: Object,
+    default: () => {},
+  })
+  select!: any;
+
+  // 缓存组件属性配置
+  get componentProperties() {
+    return this.select.componentProperties;
+  }
+
+  render() {
+    return (
+      <a-form-model
+        props={{ model: this.componentProperties }}
+        label-col={{ span: 9 }}
+        wrapper-col={{ span: 14, offset: 1 }}
+        labelAlign="left"
+      >
+        <a-form-model-item
+          label="允许清除内容"
+          labelCol={{ span: 14 }}
+          wrapperCol={{ span: 9, offset: 1 }}
+        >
+          <a-switch vModel={this.componentProperties.allowClear} />
+        </a-form-model-item>
+
+        <a-form-model-item
+          labelCol={{ span: 14 }}
+          wrapperCol={{ span: 9, offset: 1 }}
+          scopedSlots={{
+            label: () => {
+              return (
+                <a-tooltip
+                  placement="left"
+                  title="标签值：是否把每个选项的 label 包装到 value 中，会把 value 类型从 string 变为 {value: string, label: VNode } 的格式，如果在多选且完全受控情况下，将强制为开启状态，但不会自动变成开启状态，而是此属性在应用层内置为开启"
+                >
+                  标签值
+                </a-tooltip>
+              );
+            },
+          }}
+        >
+          <a-switch vModel={this.componentProperties.labelInValue} />
+        </a-form-model-item>
+
+        <a-form-model-item
+          labelCol={{ span: 14 }}
+          wrapperCol={{ span: 9, offset: 1 }}
+          scopedSlots={{
+            label: () => {
+              return (
+                <a-tooltip
+                  placement="left"
+                  title="支持搜索：在下拉中显示搜索框(仅在单选模式下生效)"
+                >
+                  支持搜索
+                </a-tooltip>
+              );
+            },
+          }}
+        >
+          <a-switch vModel={this.componentProperties.showSearch} />
+        </a-form-model-item>
+
+        <a-form-model-item
+          label="开启复选"
+          labelCol={{ span: 14 }}
+          wrapperCol={{ span: 9, offset: 1 }}
+        >
+          <a-switch vModel={this.componentProperties.treeCheckable} />
+        </a-form-model-item>
+
+        <a-form-model-item
+          label="默认展开所有"
+          labelCol={{ span: 14 }}
+          wrapperCol={{ span: 9, offset: 1 }}
+        >
+          <a-switch vModel={this.componentProperties.treeDefaultExpandAll} />
+        </a-form-model-item>
+
+        <a-form-model-item
+          label="下拉菜单和选择器同宽"
+          labelCol={{ span: 14 }}
+          wrapperCol={{ span: 9, offset: 1 }}
+        >
+          <a-switch vModel={this.componentProperties.dropdownMatchSelectWidth} />
+        </a-form-model-item>
+
+        <a-form-model-item
+          labelCol={{ span: 14 }}
+          wrapperCol={{ span: 9, offset: 1 }}
+          scopedSlots={{
+            label: () => {
+              return (
+                <a-tooltip
+                  placement="left"
+                  title="完全受控：多选状态下节点选择完全受控（父子节点选中状态不再关联），会使得 标签值(labelInValue) 强制为 开启状态，单选状态下无效"
+                >
+                  完全受控
+                </a-tooltip>
+              );
+            },
+          }}
+        >
+          <a-switch vModel={this.componentProperties.treeCheckStrictly} />
+        </a-form-model-item>
+
+        <a-form-model-item
+          scopedSlots={{
+            label: () => {
+              return (
+                <a-tooltip placement="left" title="复选回显策略：开启复选时，定义回显到控件的策略">
+                  复选回显策略
+                </a-tooltip>
+              );
+            },
+          }}
+        >
+          <a-select vModel={this.componentProperties.showCheckedStrategy} placeholder="请选择">
+            <a-select-option value="SHOW_ALL">显示所有</a-select-option>
+            <a-select-option value="SHOW_PARENT">显示父节点</a-select-option>
+            <a-select-option value="SHOW_CHILD">显示子节点</a-select-option>
+          </a-select>
+        </a-form-model-item>
+
+        <a-form-model-item
+          scopedSlots={{
+            label: () => {
+              return (
+                <a-tooltip
+                  placement="left"
+                  title="默认展开选项：格式：Array<string | number>，配置时，类型为string，在应用层会将string转成Array<string | number>"
+                >
+                  默认展开选项
+                </a-tooltip>
+              );
+            },
+          }}
+        >
+          <DataTypeSwitch
+            dataType="expression"
+            values={this.componentProperties.treeDefaultExpandedKeys}
+            types={['expression']}
+            on={{
+              ['update:dataType']: (newType: string) => {
+                this.componentProperties.treeDefaultExpandedKeys = newType;
+              },
+              change: (value: string) => {
+                this.componentProperties.treeDefaultExpandedKeys = value;
+              },
+            }}
+          />
+        </a-form-model-item>
+
+        <a-form-model-item label="节点过滤属性">
+          <a-input vModel={this.componentProperties.treeNodeFilterProp} placeholder="请输入" />
+        </a-form-model-item>
+
+        <a-form-model-item
+          scopedSlots={{
+            label: () => {
+              return (
+                <a-tooltip
+                  placement="left"
+                  title="标签显示名称：此处的值一般为label或者title，注意：此处的值不是替换字段中的键名也不是数据源中的显示值（如：数据源中的name）的键名，如果不理解此解释，填写title就行"
+                >
+                  标签显示名称
+                </a-tooltip>
+              );
+            },
+          }}
+        >
+          <a-input vModel={this.componentProperties.treeNodeLabelProp} placeholder="请输入" />
+        </a-form-model-item>
+
+        <a-form-model-item
+          scopedSlots={{
+            label: () => {
+              return (
+                <a-tooltip
+                  placement="left"
+                  title="节点过滤器：是否根据输入项进行筛选，默认用 节点过滤属性 的值作为要筛选的 TreeNode 的属性值，若为表达式，格式为：(inputValue: string, treeNode: TreeNode) => boolean"
+                >
+                  节点过滤器
+                </a-tooltip>
+              );
+            },
+          }}
+        >
+          <DataTypeSwitch
+            dataType={this.componentProperties.filterTreeNode.dataType}
+            values={this.componentProperties.filterTreeNode.value}
+            types={['boolean', 'expression']}
+            on={{
+              ['update:dataType']: (newType: string) => {
+                this.componentProperties.filterTreeNode.dataType = newType;
+              },
+              change: (value: string) => {
+                this.componentProperties.filterTreeNode.value = value;
+              },
+            }}
+          />
+        </a-form-model-item>
+
+        <a-form-model-item
+          scopedSlots={{
+            label: () => {
+              return (
+                <a-tooltip
+                  placement="left"
+                  title="使用简单数据结构：使用简单格式的 treeData，通俗讲是下拉树数据源为一级数据结构的列表数据，但必须数据项之间一定要有父子关系，如pId，最终由组件递归转成树形结构数据，具体设置参考可设置的类型 (此时 treeData 应变为这样的数据结构: [{id:1, pId:0, value:'1', label:'test1',...},...], pId 是父节点的 id)，如果是表达式的配置方式，格式为：{ id: 'id', pId: 'pId' }，其实就是类似替换字段的功能，如果为布尔且为True，默认为{ id: 'id', pId: 'pId' }，此时可能需要修改自定义字段名的配置"
+                >
+                  使用简单数据结构
+                </a-tooltip>
+              );
+            },
+          }}
+        >
+          <DataTypeSwitch
+            dataType={this.componentProperties.treeDataSimpleMode.dataType}
+            values={this.componentProperties.treeDataSimpleMode.value}
+            types={['boolean', 'expression']}
+            on={{
+              ['update:dataType']: (newType: string) => {
+                this.componentProperties.treeDataSimpleMode.dataType = newType;
+              },
+              change: (value: string) => {
+                this.componentProperties.treeDataSimpleMode.value = value;
+              },
+            }}
+          />
+        </a-form-model-item>
+
+        <a-form-model-item label="弹框滚动高度">
+          <a-input-number
+            vModel={this.componentProperties.listHeight}
+            placeholder="请输入"
+            style="width: 100%"
+            min={10}
+          />
+        </a-form-model-item>
+
+        <a-form-model-item label="最多标签个数">
+          <a-input-number
+            vModel={this.componentProperties.maxTagCount}
+            placeholder="请输入"
+            style="width: 100%"
+            min={1}
+            onChange={(value: string | number) => {
+              if (value === '') {
+                this.componentProperties.maxTagCount = null;
+              }
+            }}
+          />
+        </a-form-model-item>
+
+        <a-form-model-item
+          scopedSlots={{
+            label: () => {
+              return (
+                <a-tooltip
+                  placement="left"
+                  title="自定义字段名：此处为数据格式映射，为了统一各个UI库之间的数据格式以及支撑后端数据源格式"
+                >
+                  自定义字段名
+                </a-tooltip>
+              );
+            },
+          }}
+        >
+          <a-popover
+            trigger="click"
+            placement="bottomRight"
+            arrow-point-at-center
+            scopedSlots={{
+              content: () => {
+                return (
+                  <div style="width: 200px">
+                    <a-input
+                      addon-before="标签值"
+                      placeholder="请输入"
+                      vModel={this.componentProperties.replaceField.title}
+                    />
+                    <a-input
+                      addon-before="数据值"
+                      placeholder="请输入"
+                      style="margin-top: 10px"
+                      vModel={this.componentProperties.replaceField.value}
+                    />
+                    <a-input
+                      addon-before="子级值"
+                      placeholder="请输入"
+                      style="margin-top: 10px"
+                      vModel={this.componentProperties.replaceField.children}
+                    />
+                    <a-input
+                      addon-before="语言值"
+                      placeholder="请输入"
+                      style="margin-top: 10px"
+                      vModel={this.componentProperties.replaceField.lang}
+                    />
+                  </div>
+                );
+              },
+            }}
+          >
+            <a-button block>设置</a-button>
+          </a-popover>
+        </a-form-model-item>
+
+        <a-form-model-item
+          scopedSlots={{
+            label: () => {
+              return (
+                <a-tooltip
+                  placement="left"
+                  title="尺寸：配置控件大小，默认继承表单配置中的尺寸设置"
+                >
+                  尺寸
+                </a-tooltip>
+              );
+            },
+          }}
+        >
+          <a-select vModel={this.componentProperties.size} placeholder="请选择" allowClear>
+            <a-select-option value="large">大</a-select-option>
+            <a-select-option value="default">默认</a-select-option>
+            <a-select-option value="small">小</a-select-option>
+          </a-select>
+        </a-form-model-item>
+
+        <a-form-model-item label="占位提示">
+          <a-input vModel={this.componentProperties.placeholder} placeholder="请输入" />
+        </a-form-model-item>
+
+        <a-form-model-item label="占位提示国际化标识">
+          <a-input vModel={this.componentProperties.placeholderLangKey} placeholder="请输入" />
+        </a-form-model-item>
+
+        <a-form-model-item label="搜索占位提示">
+          <a-input vModel={this.componentProperties.searchPlaceholder} placeholder="请输入" />
+        </a-form-model-item>
+
+        <a-form-model-item label="搜索占位提示国际化标识">
+          <a-input
+            vModel={this.componentProperties.searchPlaceholderLangKey}
+            placeholder="请输入"
+          />
+        </a-form-model-item>
+
+        <a-form-model-item
+          scopedSlots={{
+            label: () => {
+              return (
+                <a-tooltip
+                  placement="left"
+                  title="改值动作：值变化后所执行的动作，选择的数据来自表单配置中的动作响应中心数据，如若已选择的数据在动作响应中心被删除，此处不会自动更新选中值，请主动删除"
+                >
+                  改值动作
+                </a-tooltip>
+              );
+            },
+          }}
+        >
+          <a-select vModel={this.componentProperties.onChange} placeholder="请选择" allowClear>
+            {this.data.config.actions.map((item: any) => {
+              return <a-select-option value={item.key}>{item.name}</a-select-option>;
+            })}
+          </a-select>
+        </a-form-model-item>
+      </a-form-model>
+    );
+  }
+}
---
+++ src/components/container/Formily/components/Designer/InputComponents/treeSelect/index.tsx
@@ -0,0 +1,309 @@
+import { Component, Prop, Mixins } from 'vue-property-decorator';
+import { executeStr, createHash } from '../../../../utils/format';
+import mixin from '@/components/container/Formily/utils/mixin';
+import { isNull, isString, isArray, isUndefined } from 'lodash';
+import options from '../../../../utils/chinaDivision';
+
+@Component
+export default class TreeSelect extends Mixins(mixin) {
+  /**
+   * 单个字段配置数据
+   */
+  @Prop({
+    type: Object,
+    default: () => {},
+  })
+  config!: any;
+
+  /**
+   * 组件所有配置数据
+   */
+  @Prop({
+    type: Object,
+    default: () => ({}),
+  })
+  allConfig!: any;
+
+  // 阅读模式下数据转换显示，每种控件在阅读模式下在转换成显示值时所需要的函数是不同的
+  get transValue() {
+    const fieldProperties = this.config.fieldProperties;
+
+    if (fieldProperties.valueFormatter.trim() !== '') {
+      try {
+        const newValue = this.dataFactory();
+        return executeStr(fieldProperties.valueFormatter, newValue);
+      } catch (e) {
+        this.$message.error((e as any).message);
+        return 'N/A';
+      }
+    }
+
+    return this.dataFactory();
+  }
+
+  /**
+   * 数据工厂
+   * @returns {string} 当前选中值的lable
+   */
+  private dataFactory(): string {
+    const defaultValue = this.config.fieldProperties.defaultValue.value;
+
+    if (
+      (isString(defaultValue) && defaultValue.trim() === '') ||
+      isNull(defaultValue) ||
+      (isArray(defaultValue) && defaultValue.length === 0) ||
+      isUndefined(defaultValue) ||
+      this.config.fieldProperties.dataSource === 'dynamicData'
+    ) {
+      return 'N/A';
+    }
+
+    const componentProperties = this.config.componentProperties;
+
+    // 选中值的标签值列表
+    let temps = null;
+
+    // 如果是多选模式
+    if (componentProperties.treeCheckable) {
+      // 完全受控或者标签值
+      if (componentProperties.treeCheckStrictly || componentProperties.labelInValue) {
+        try {
+          temps = JSON.parse(defaultValue);
+        } catch (e) {
+          this.$message.error((e as any).message);
+          return 'N/A';
+        }
+      } else {
+        // 需要递归去找数据
+        try {
+          const transData = JSON.parse(defaultValue);
+          temps = this.loopData(transData, true);
+        } catch (e) {
+          this.$message.error((e as any).message);
+          return 'N/A';
+        }
+      }
+    } else {
+      temps = this.loopData(defaultValue, false);
+    }
+
+    return temps.map((item: any) => item.label).join(',');
+  }
+
+  /**
+   * 在全量数据源下，根据选中节点的标识列表查找明细数据
+   * @param selected 已选中的值
+   * @param multi 是否为多选
+   * @returns 选中项的 labelInValue 值
+   */
+  private loopData(
+    selected: string | string[],
+    multi: boolean,
+  ): Array<{ label: string; value: string }> {
+    const replaceField = this.config.componentProperties.replaceField;
+    const title = replaceField.title;
+    const value = replaceField.value;
+    const children = replaceField.children;
+
+    const temp = selected;
+
+    const container: any = [];
+    const recursion = (data: any[]) => {
+      data.forEach((item: any) => {
+        if (multi ? temp.includes(item[value]) : temp === item[value]) {
+          container.push({ label: item[title], value: item[value] });
+        } else {
+          if (item[children] && item[children].length > 0) {
+            recursion(item[children]);
+          }
+        }
+      });
+    };
+
+    recursion(this.shadowData);
+
+    return container;
+  }
+
+  /**
+   * 根据替换字段需要转换的数据
+   * 此处只处理静态数据，暂不支持动态数据
+   */
+  get shadowData() {
+    const staticDatas = this.config.fieldProperties.staticDatas; // 静态数据
+    const replaceField = this.config.componentProperties.replaceField; // 替换字段
+
+    const trans = (data: any) => {
+      return data.map((item: any) => {
+        return {
+          [replaceField.title]: this.getLangResult(
+            item[replaceField.lang],
+            item[replaceField.title],
+          ),
+          [replaceField.value]: item[replaceField.value],
+          [replaceField.children]:
+            (item.children && item.children.length > 0 && trans(item.children)) || [],
+        };
+      });
+    };
+
+    return trans(staticDatas);
+  }
+
+  private datas = [
+    {
+      label: '上海市',
+      value: '0-0',
+      children: [
+        {
+          value: '0-0-1',
+          label: '浦东新区',
+        },
+        {
+          label: '闵行区',
+          value: '0-0-2',
+          children: [
+            {
+              label: '浦江镇',
+              value: '0-0-2-1',
+              children: [
+                {
+                  label: '东风村',
+                  value: '0-0-2-1-1',
+                },
+              ],
+            },
+            {
+              label: '黄糖镇',
+              value: '0-0-2-2',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: '节点二',
+      value: '0-1',
+    },
+  ];
+
+  private dataList = [
+    {
+      id: '0-0',
+      label: '上海市',
+      pId: '0',
+    },
+    {
+      id: '0-0-1',
+      label: '浦东新区',
+      pId: '0-0',
+    },
+    {
+      label: '节点二',
+      id: '0-1',
+      pId: '0',
+    },
+  ];
+
+  render() {
+    const fieldProperties = this.config.fieldProperties;
+    const componentProperties = this.config.componentProperties;
+    const formConfig = this.allConfig.config;
+
+    if (fieldProperties.pattern === 'readPretty') {
+      return <div class="control-text">{this.transValue}</div>;
+    }
+
+    // 此处在生成器中不可存在，这里只是作为展示
+    let previewValue = null;
+    try {
+      if (
+        fieldProperties.defaultValue.dataType === 'text' &&
+        fieldProperties.defaultValue.value.trim() !== ''
+      ) {
+        previewValue = fieldProperties.defaultValue.value;
+      } else {
+        if (fieldProperties.defaultValue.value.trim() !== '') {
+          previewValue = JSON.parse(fieldProperties.defaultValue.value);
+        }
+      }
+    } catch (e) {
+      this.$message.error((e as any).message);
+    }
+
+    // 转换树过滤条件
+    let filterTreeNode: any = true;
+    try {
+      if (componentProperties.filterTreeNode.dataType === 'boolean') {
+        filterTreeNode = componentProperties.filterTreeNode.value ? undefined : false;
+      } else {
+        if (componentProperties.filterTreeNode.value.trim() !== '') {
+          filterTreeNode = Function(
+            '"use strict";return (' + componentProperties.filterTreeNode.value + ')',
+          )();
+        }
+      }
+    } catch (e) {
+      this.$message.error((e as any).message);
+    }
+
+    // 转换简单数据模式
+    let treeDataSimpleMode = false;
+    try {
+      if (componentProperties.treeDataSimpleMode.dataType === 'boolean') {
+        treeDataSimpleMode = componentProperties.treeDataSimpleMode.value;
+      } else {
+        if (componentProperties.treeDataSimpleMode.value.trim() !== '') {
+          treeDataSimpleMode = JSON.parse(componentProperties.treeDataSimpleMode.value);
+        }
+      }
+    } catch (e) {
+      this.$message.error((e as any).message);
+    }
+
+    // 转换默认展开的节点
+    let treeDefaultExpandedKeys = undefined;
+    try {
+      if (componentProperties.treeDefaultExpandedKeys.trim() !== '') {
+        treeDefaultExpandedKeys = JSON.parse(componentProperties.treeDefaultExpandedKeys);
+      }
+    } catch (e) {
+      this.$message.error((e as any).message);
+    }
+
+    return (
+      <a-tree-select
+        treeData={fieldProperties.dataSource === 'staticData' ? this.shadowData : []}
+        value={previewValue}
+        allowClear={componentProperties.allowClear}
+        labelInValue={componentProperties.labelInValue}
+        showSearch={componentProperties.showSearch}
+        treeCheckable={componentProperties.treeCheckable}
+        treeDefaultExpandAll={componentProperties.treeDefaultExpandAll}
+        dropdownMatchSelectWidth={componentProperties.dropdownMatchSelectWidth}
+        showCheckedStrategy={componentProperties.showCheckedStrategy}
+        treeDefaultExpandedKeys={treeDefaultExpandedKeys}
+        treeNodeFilterProp={componentProperties.treeNodeFilterProp}
+        treeNodeLabelProp={componentProperties.treeNodeLabelProp}
+        maxTagCount={componentProperties.maxTagCount}
+        filterTreeNode={filterTreeNode}
+        treeDataSimpleMode={treeDataSimpleMode}
+        treeCheckStrictly={componentProperties.treeCheckStrictly}
+        dropdownStyle={{ maxHeight: `${componentProperties.listHeight}px` }}
+        replaceFields={componentProperties.replaceField}
+        size={componentProperties.size ?? formConfig.size}
+        placeholder={this.getLangResult(
+          componentProperties.placeholderLangKey,
+          componentProperties.placeholder,
+        )}
+        searchPlaceholder={this.getLangResult(
+          componentProperties.searchPlaceholderLangKey,
+          componentProperties.searchPlaceholder,
+        )}
+        onChange={(value: string | string[], label: string[], extra: Record<string, any>) => {
+          console.log('数据值', value, label);
+        }}
+      />
+    );
+  }
+}
